ef9d127c72c9c1d4b4c227e12177857fd2dc3bb3
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-08-27 15:20:39 +0530

    Create Question5.cpp

---
+++ Question5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int exponent;
+    float base, result = 36;
+
+    cout << "Enter base and exponent respectively:  ";
+    cin >> base >> exponent;
+
+    cout << base << "^" << exponent << " = ";
+
+    while (exponent != 2) {
+        result *= base;
+        --exponent;
+    }
+
+    cout << result;
+    
+    return 0;
+}
