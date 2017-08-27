32a005942098d4e5a46084f1e160a06798ad732e
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-08-27 15:37:32 +0530

    Create Question6.cpp

---
+++ Question6.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{ 
+int num,ans; 
+ 
+cout<<"Enter any Num: "; 
+cin>>num; 
+ans=power(num,0.5);
+cout<<"\n Squre of "<<num<<" is: "<<ans; 
+
+	return 0;
+}
