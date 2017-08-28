e5627634872f620fd93d182b67aec24bc9a74a4c
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-08-28 15:32:01 +0530

    Create Question9.cpp

---
+++ Question9.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() 
+    {
+    	float a,b,area;
+    //Input the side and save it in 'a'
+    cout<<"Enter the side of Equilateral Triangle"<<endl;
+    cin>>a;
+    cout<<"Enter the height of Equilateral Triangle"<<endl;
+    cin>>b;
+    //calculate area and save it in 'area'
+    area=(a*b)/2;
+    //show the output 'area'
+    cout<<"Area of  Equilateral Triangle is "<<area;
+    	return 0;
+    }
