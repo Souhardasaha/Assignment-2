1c62927a5c12740445ddd503a66fecdd36553c62
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-08-28 15:24:47 +0530

    Create Question8.cpp

---
+++ Question8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	float base , height , area;
+
+
+
+ cout<<"Enter base of Triangle  ";
+ cin>>base;
+   
+ cout<<"Enter height of Triangle ";
+ cin>>height;
+ 
+  
+ area = 0.5 * base * height ;
+  
+ cout<<"Area of Triangle is  =  "<<area;
+ 
+	return 0;
+}
