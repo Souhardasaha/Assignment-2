a0d676c153a1c04f83a39c2b781e67505dc3acb6
Souhardasaha <31206017+Souhardasaha@users.noreply.github.com>
2017-08-28 15:41:53 +0530

    Create Question7.cpp

---
+++ Question7.cpp
@@ -0,0 +1,19 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() 
+    {
+    	float angle1,angle2,angle3;
+    	cout<<"Enter angle1 of Triangle";
+    	cin>>angle1;
+     
+    	cout<<"Enter angle2 of Triangle";
+    	cin>>angle2;
+     
+    	angle3=180-(angle1+angle2);
+     
+    	cout<<"angle3 of triangle is= "<<angle3;
+     
+     
+    	return 0;
+    }
